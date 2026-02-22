e49eb7dee8834c03d2c9108d7036bb1f943a4834
Green Energy Perú <greenenergyperu1973@gmail.com>
2026-02-21 22:30:34 -0500

    Se a agregado la direcion del servidor web hosting

--- app/src/main/java/pe/edu/cibertec/recetario/core/Config.kt
+++ app/src/main/java/pe/edu/cibertec/recetario/core/Config.kt
@@ -2,8 +2,8 @@ package pe.edu.cibertec.recetario.core
 
 object Config {
     // CAMBIA AQUÍ LA IP PARA TODO EL PROYECTO
-    private const val IP = "192.168.1.30"
+    private const val IP = "greenenergyperu.com"
 
-    const val BASE_URL = "http://$IP/recetario_api/"
+    const val BASE_URL = "http://$IP/recetaapi/"
     const val ACTUAL_IP = IP
 }
